4d1663494c9b45caed69a2f4e60d18bb6071e2ca
thiago <thiagot1234@hotmail.com>
2021-05-20 11:54:48 -0300

    criando class PageAdmin e tpl do admin

--- index.php
+++ index.php
@@ -5,18 +5,28 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 //buscando o corpo do site.'header''footer'
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 //daqui pra cima o nescessario para cria a pagina
 
+//primeira rota
 $app->get('/', function() {
 	//tem q cria a variavel Page
 	$page = new Page();//chama o __construct, o header
 
 	$page->setTpl("index");//chama o conteudo. por exemplo o 'h1'
 	//__destruct e automatico
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");//chama o conteudo da page.
+
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hcode;
+
+/**
+ *
+ */
+class PageAdmin extends Page
+{
+
+  public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+  {
+    //chamando o __construct da class Page
+    parent::__construct($opts, $tpl_dir);
+
+  }
+
+}
+
+
+ ?>
