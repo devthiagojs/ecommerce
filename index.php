e76b260f9b1c372d5f1a3f59072ae7e0b6d0895d
thiago <thiagot1234@hotmail.com>
2021-05-20 11:18:45 -0300

    tpl site

--- index.php
+++ index.php
@@ -2,14 +2,21 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+//buscando o corpo do site.'header''footer'
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
+//daqui pra cima o nescessario para cria a pagina
 
 $app->get('/', function() {
+	//tem q cria a variavel Page
+	$page = new Page();//chama o __construct, o header
 
-	echo "OK";
-
+	$page->setTpl("index");//chama o conteudo. por exemplo o 'h1'
+	//__destruct e automatico
 });
 
 $app->run();
